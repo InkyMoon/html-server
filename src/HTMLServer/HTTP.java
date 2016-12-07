cd24f5f0c7c6c4df334e69b7f14d97eac8a85ac4
InkyMoon <patrick.kastelic@gmail.com>
2016-12-07 14:07:12 -0600

    Formatting and initial work for supporting multiple types of HTTP commands

--- src/HTMLServer/HTTP.java
+++ src/HTMLServer/HTTP.java
@@ -10,98 +10,94 @@ import java.io.IOException;
  *
  * @author Patrick Kastelic <patrick.kastelic@gmail.com>
  */
-public class HTTP
-{
+public class HTTP {
 
-   private static final String CRLF = "\r\n";
-   private static final String HTTP_VER = "HTTP/1.0";
-   private static final int CHUNK_SIZE = 1024;
+    private static final String CRLF = "\r\n";
+    private static final String HTTP_VER = "HTTP/1.0";
+    private static final int CHUNK_SIZE = 1024;
 
-   /**
-    * Responds to a HTTP GET command.
-    *
-    * @param address is the path to the file
-    * @param output is the DataOutputStream to send the response message to
-    * @throws IOException if unable to write to the output stream
-    */
-   public static void get(String address, DataOutputStream output)
-         throws IOException
-   {
-      address = "." + address;
-      boolean found = true;
-      FileInputStream file = null;
-      try
-      {
-         file = new FileInputStream(address);
-      }
-      catch (FileNotFoundException ex)
-      {
-         found = false;
-      }
-      String status = HTTP_VER + (found ? " 200 OK" : " 404 Not Found");
-      status = status + CRLF;
-      output.writeBytes(status);
-      String header = "Content-Type: " + contentType(address);
-      header = header + CRLF + "Connection: close";
-      header = header + CRLF;
-      output.writeBytes(header);
-      if (found)
-      {
-         writeFile(file, output);
-      }
-      else
-      {
-         output.writeBytes("<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>"
-               + "<BODY>Not Found</BODY></HTML>");
-      }
-   }
+    /**
+     * Responds to a HTTP GET command.
+     *
+     * @param address is the path to the file
+     * @param output is the DataOutputStream to send the response message to
+     * @throws IOException if unable to write to the output stream
+     */
+    public static void get(String address, DataOutputStream output)
+            throws IOException {
+        address = "." + address;
+        boolean found = true;
+        FileInputStream file = null;
+        try
+        {
+            file = new FileInputStream(address);
+        }
+        catch (FileNotFoundException ex)
+        {
+            found = false;
+        }
+        String status = HTTP_VER + (found ? " 200 OK" : " 404 Not Found");
+        status = status + CRLF;
+        output.writeBytes(status);
+        String header = "Content-Type: " + contentType(address);
+        header = header + CRLF + "Connection: close";
+        header = header + CRLF;
+        output.writeBytes(header);
+        if (found)
+        {
+            writeFile(file, output);
+        }
+        else
+        {
+            output.writeBytes("<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>"
+                              + "<BODY>Not Found</BODY></HTML>");
+        }
+    }
 
-   /**
-    * Writes a file to an output stream in small chunks.
-    *
-    * @param fileI the file input stream
-    * @param out the output stream to write to
-    * @throws IOException if the stream cannot be written
-    */
-   private static void writeFile(FileInputStream fileI, DataOutputStream out)
-         throws IOException
-   {
-      out.writeBytes(CRLF);
-      int bytesToRead = fileI.available();
-      byte[] chunk = new byte[CHUNK_SIZE];
-      while (bytesToRead != 0)
-      {
-         bytesToRead = bytesToRead < CHUNK_SIZE ? bytesToRead : CHUNK_SIZE;
-         fileI.read(chunk, 0, bytesToRead);
-         out.write(chunk, 0, bytesToRead);
-         bytesToRead = fileI.available();
-      }
-   }
+    /**
+     * Writes a file to an output stream in small chunks.
+     *
+     * @param fileI the file input stream
+     * @param out the output stream to write to
+     * @throws IOException if the stream cannot be written
+     */
+    private static void writeFile(FileInputStream fileI, DataOutputStream out)
+            throws IOException {
+        out.writeBytes(CRLF);
+        int bytesToRead = fileI.available();
+        byte[] chunk = new byte[CHUNK_SIZE];
+        while (bytesToRead != 0)
+        {
+            bytesToRead = bytesToRead < CHUNK_SIZE ? bytesToRead : CHUNK_SIZE;
+            fileI.read(chunk, 0, bytesToRead);
+            out.write(chunk, 0, bytesToRead);
+            bytesToRead = fileI.available();
+        }
+    }
 
-   /**
-    * Determines the content type attribute of a request.
-    *
-    * @param fileName the file to examine
-    * @return the content type of the file
-    */
-   private static String contentType(String fileName)
-   {
-      if (fileName.endsWith(".htm") || fileName.endsWith(".html"))
-      {
-         return "text/html";
-      }
-      if (fileName.endsWith(".gif"))
-      {
-         return "image/gif";
-      }
-      if (fileName.endsWith(".bmp"))
-      {
-         return "image/bmp";
-      }
-      if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg"))
-      {
-         return "image/jpeg";
-      }
-      return "application/octet-stream";
-   }
+    /**
+     * Determines the content type attribute of a request.
+     *
+     * @param fileName the file to examine
+     * @return the content type of the file
+     */
+    private static String contentType(String fileName) {
+        if (fileName.endsWith(".htm") || fileName.endsWith(".html"))
+        {
+            return "text/html";
+        }
+        if (fileName.endsWith(".gif"))
+        {
+            return "image/gif";
+        }
+        if (fileName.endsWith(".bmp"))
+        {
+            return "image/bmp";
+        }
+        if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg"))
+        {
+            return "image/jpeg";
+        }
+        return "application/octet-stream";
+    }
 }
--- src/HTMLServer/HTTPRequest.java
+++ src/HTMLServer/HTTPRequest.java
@@ -8,82 +8,83 @@ import java.net.Socket;
 import java.util.StringTokenizer;
 
 /**
- * A HTTP 1.0 server thread.
- * Handles a single HTTP request. Currently supports only the GET command, all
- * other commands will print an error and disconnect from the client.
+ * A HTTP 1.0 server thread. Handles a single HTTP request. Currently supports
+ * only the GET command, all other commands will print an error and disconnect
+ * from the client.
  *
  * @author Patrick Kastelic <patrick.kastelic@gmail.com>
  */
-public class HTTPRequest extends Thread
-{
+public class HTTPRequest extends Thread {
 
-   private Socket sock;
-   private DataOutputStream writeSock;
-   private BufferedReader readSock;
+    private Socket sock;
+    private DataOutputStream writeSock;
+    private BufferedReader readSock;
 
-   /**
-    * HTTPRequest constructor.
-    * @param clientSock the socket representing the client connection
-    * @throws IOException if I/O cannot be established with the client
-    */
-   public HTTPRequest(Socket clientSock)
-         throws IOException
-   {
-      sock = clientSock;
-      writeSock = new DataOutputStream(sock.getOutputStream());
-      readSock = new BufferedReader(
-            new InputStreamReader(sock.getInputStream()));
-   }
+    /**
+     * HTTPRequest constructor.
+     *
+     * @param clientSock the socket representing the client connection
+     * @throws IOException if I/O cannot be established with the client
+     */
+    public HTTPRequest(Socket clientSock)
+            throws IOException {
+        sock = clientSock;
+        writeSock = new DataOutputStream(sock.getOutputStream());
+        readSock = new BufferedReader(
+                new InputStreamReader(sock.getInputStream()));
+    }
 
-   /**
-    * Runs the thread.
-    * Prints errors to the system console.
-    */
-   public void run()
-   {
-      String request = null;
-      boolean disconnect = false;
-      try
-      {
-         request = readSock.readLine();
-         if (request == null)
-         {
+    /**
+     * Runs the thread. Prints errors to the system console.
+     */
+    public void run() {
+        String request = null;
+        boolean disconnect = false;
+        try
+        {
+            request = readSock.readLine();
+            if (request == null)
+            {
+                disconnect = true;
+            }
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not read request from client.");
             disconnect = true;
-         }
-      }
-      catch (IOException ex)
-      {
-         System.out.println("Could not read request from client.");
-         disconnect = true;
-      }
-      if (!disconnect)
-      {
-         System.out.println("Request " + request + " on port " + sock.getPort());
-         StringTokenizer st = new StringTokenizer(request);
-         String command = st.nextToken();
-         if (!command.equals("GET"))
-         {
-            System.out.println("Unsupported command received: " + command);
-         }
-         else
-         {
-            try
+        }
+        if (!disconnect)
+        {
+            System.out.println("Request " + request + " on port " + sock.getPort());
+            StringTokenizer st = new StringTokenizer(request);
+            String command = st.nextToken();
+            switch (command)
             {
-               HTTP.get(st.nextToken(), writeSock);
+                case "GET":
+                    Get(st.nextToken());
+                    break;
+                default:
+                    System.out.println("Unsupported command received: " + command);
             }
-            catch (IOException ex)
-            {
-               System.out.println("Could not send file to client.");
-            }
-         }
-      }
-      try
-      {
-         sock.close();
-      }
-      catch (IOException ex)
-      {
-         System.out.println("Could not close client socket. Already closed.");
-      }
-   }
+        }
+        try
+        {
+            sock.close();
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not close client socket. Already closed.");
+        }
+    }
+
+    private void Get(String filepath) {
+        try
+        {
+            HTTP.get(filepath, writeSock);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not send file to client.");
+        }
+    }
 }
